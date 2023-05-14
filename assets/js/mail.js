480ad53155c23e0740c0b9de6f19ff549d5aa643
Kevin Wang (Contractor) <wwang1@woolworths.com.au>
2023-05-15 09:52:20 +1000

    try

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -1,11 +1,16 @@
 $("form").on("submit", function (e) {
-    var dataString = $(this).serialize();
     var name = $("#name").val();
     var email = $("#email").val();
     var message = $("#message").val();
-    console.log('name', name);
-    console.log('email', email);
-    console.log('message', message);
+    var data = {
+        contactName: name,
+        contactEmail: email,
+        message: message
+    };
+
+    var json = JSON.stringify(data);
+    console.log('json', json);
+
     if(name == "" || email == "" || message == ""){
         $(".failed-message").html('We received your message and we will get back to you asap!');
     }
@@ -18,12 +23,8 @@ $("form").on("submit", function (e) {
             },
             type: "POST",
             crossDomain: true,
-            url: "https://preciseagency-mailer.netlify.app/.netlify/functions/send-contact-email",
-            data: {
-              contactName: name,
-              contactEmail: email,
-              message: message
-            },
+            url: "https://fiddle.jshell.net/echo/json/",
+            data: json,
             dataType: 'json',
             success: function () {
               // Display message back to the user here 
