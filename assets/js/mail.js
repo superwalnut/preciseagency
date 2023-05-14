ed3a232229ddcfea4d586af71615a3bf947eb422
Kevin Wang (Contractor) <wwang1@woolworths.com.au>
2023-05-14 17:40:29 +1000

    change message style

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -3,18 +3,25 @@ $("form").on("submit", function (e) {
     var name = $("#name").val();
     var email = $("#email").val();
     var message = $("#message").val();
-    $.ajax({
-      type: "POST",
-      url: "https://preciseagency-mailer.netlify.app/.netlify/functions/send-contact-email",
-      data: {
-        contactName: name,
-        contactEmail: email,
-        message: message
-      },
-      success: function () {
-        // Display message back to the user here 
-        $(".success-message").html('We received your message and we will get back to you asap!');
-      }
-    });
+
+    if(name == "" || email == "" || message == ""){
+        $(".failed-message").html('We received your message and we will get back to you asap!');
+    }
+    else{
+        $.ajax({
+            type: "POST",
+            url: "https://preciseagency-mailer.netlify.app/.netlify/functions/send-contact-email",
+            data: {
+              contactName: name,
+              contactEmail: email,
+              message: message
+            },
+            success: function () {
+              // Display message back to the user here 
+              $(".success-message").html('We received your message and we will get back to you asap!');
+            }
+          });
+    }
+    
     e.preventDefault();
 });
